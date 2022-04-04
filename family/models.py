ab9a3971d13be511ba680508df5920fc0bb601ae
harsh kumar <201211@iiitt.ac.in>
2022-04-04 10:33:40 +0530

    created models

--- family/models.py
+++ family/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Parents(models.Model):
+     Husband_Name = models.CharField(max_length= 100)
+     Wife_name = models.CharField(max_length= 100)
+
+
+class Childrens(models.Model):
+    parent_id = models.ForeignKey(Parents, on_delete= models.CASCADE)
+    Child_Name = models.CharField(max_length=100)
